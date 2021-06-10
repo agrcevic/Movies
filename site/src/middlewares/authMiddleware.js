155f03beefc3f5aa5642c1af6d656484e5414f0f
agrcevic <74520182+agrcevic@users.noreply.github.com>
2021-06-10 16:39:47 -0300

    arreglos en middlewares

---
+++ site/src/middlewares/authMiddleware.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+    //validaciones para saber si es administrador o usuario comun. esto sirve para saber si puede o no editar un producto, o crear producto 
+    if (req.session.user && req.session.auth) {
+        next();
+    }else{
+        return res.redirect('/');//redireccionamiento por si no cumple ser administrador
+    } 
+};
--- site/src/routes/usersRouter.js
+++ site/src/routes/usersRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require ('../controllers/usersController');
-//const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/listUsers', authMiddleware, userController.listUsers);//para saber si tiene permiso para ver la lista de usuarios
 
